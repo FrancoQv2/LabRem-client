9f61b9eb4f24a9ea59022ab5e20f913bb3435d76
Franco Quevedo <francoq96@gmail.com>
2023-05-28 00:41:40 -0300

    Actualizados los archivos correspondientes a Digital - UART

--- src/components/digital/FormUART.jsx
+++ src/components/digital/FormUART.jsx
@@ -1,140 +1,136 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
 
-import FormSelect from "../common/FormSelect";
-import FormSaveUART from "./FormSaveUART";
-import { usePostEnsayoUART } from "../../hooks/digital";
-import { submitSuccess, submitError } from "../../libs/alerts";
-import DownloadImage from "../DownloadImage" 
+import { usePostEnsayoUART } from "../../hooks/digital"
+import { submitSuccess, submitError } from "../../libs/alerts"
+
+import FormText from "../common/FormText"
+import FormSelect from "../common/FormSelect"
+import FormBtnGroup from "../common/FormBtnGroup"
+import BtnDownloadImage from "../common/BtnDownloadImage"
 
 function FormUART({ idUsuario }) {
-  // Definicion de valores posibles
-  const valuesVelocidad = [
-    300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600,]; // bps
-  const defaultVelocidad = valuesVelocidad[5];
-  const [cambio,setcambio] =useState(true);
 
-  const valuesParidad = ["par","impar" ]; // bps
-  const defaultParidad = valuesParidad[0];
+  // Definicion de valores de variables
 
-  const valuescantidadBitParada = [0,1,2 ]; // bps
-  const defaultcantidadBitParada = valuescantidadBitParada[0];
-
+  const valuesVelocidad = [ 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600 ] // bps
+  const defaultVelocidad = valuesVelocidad[5]
+  
+  const valuesBitsDatos = [5,6,7,8,9]
+  const defaultBitsDatos = valuesBitsDatos[2]
+  
+  const valuesParidad = ["Par","Impar"]
+  const defaultParidad = valuesParidad[0]
+  
+  const valuesBitsParada = [1,2]
+  const defaultBitsParada = valuesBitsParada[0]
+  
+  const defaultPulsadores = [0, 0, 0, 0]
+  
+  const defaultMensaje = ""
+  
   // Definicion de Hooks
-  const [velocidad, setVelocidad] = useState(defaultVelocidad);
-  const [cantidadBitDato, setcantidadBitsDato] = useState("");
-  const [paridad, setParidad] = useState(defaultParidad);
-  const [cantidadBitParada, setParada] = useState(defaultcantidadBitParada);
-  const [mensaje, setMensaje] = useState("");
 
-  const { mutate, error, isLoading } = usePostEnsayoUART();
+  const [cambio, setCambio] = useState(true)
+
+  const [velocidad, setVelocidad] = useState(defaultVelocidad)
+  const [bitsDatos, setBitsDatos] = useState(defaultBitsDatos)
+  const [paridad, setParidad] = useState(defaultParidad)
+  const [bitsParada, setBitsParada] = useState(defaultBitsParada)
+  const [pulsadores, setPulsadores] = useState(defaultPulsadores)
+  const [mensaje, setMensaje] = useState(defaultMensaje)
+
+  const { mutate, error, isLoading } = usePostEnsayoUART()
+
+  // Definicion de funciones Handle
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    setcambio(current =>!current);
-    mutate({idUsuario,velocidad,cantidadBitDato,paridad,cantidadBitParada,mensaje,setcambio },
+    e.preventDefault()
+    setCambio(current => !current)
+    mutate({ idUsuario, velocidad, bitsDatos, bitsParada, paridad, pulsadores, mensaje, setCambio},
       {
         onSuccess: () => {
-          setMensaje("");
-          submitSuccess();
+          setMensaje("")
+          submitSuccess()
         },
         onError: () => {
-          submitError();
+          submitError()
         },
       }
-    );
-  };
+    )
+  }
  
   
   return (
     <Form className="m-3" onSubmit={handleSubmit}>
+
       <FormSelect
         name="Velocidad"
         values={valuesVelocidad}
         defaultValue={defaultVelocidad}
         setState={setVelocidad}
       />
-     <Row className="my-3">
-        <Form.Group
-          className="border border-secondary rounded"
-          controlId="formcantidadbitsdato"
-          onChange={(changeEvent) => setcantidadBitsDato(changeEvent.target.value)}
-        >
-          <Row className="my-3">
-            <Col sm={4} lg={6}>
-              <span className="input-group-text" htmlFor="cantidadbitsdato">
-                 Cantidad de Bits del Dato
-              </span>
-            </Col>
-            <Col sm={4} lg={6}>
-              <Form.Control type="text" aria-describedby="text-cantidadbitsdato" />
-              <Form.Text id="text-cantidadbitsdato"></Form.Text>
-            </Col>
-          </Row>
-        </Form.Group>
-      </Row>
-      <Row className="my-3">
-        <Form.Group
-          className="border border-secondary rounded"
-          controlId="formMensaje"
-          onChange={(changeEvent) => setMensaje(changeEvent.target.value)}
-        >
-          <Row className="my-3">
-            <Col sm={4} lg={6}>
-              <span className="input-group-text" htmlFor="mensaje">
-                Caracteres a Trasmitir
-              </span>
-            </Col>
-            <Col sm={4} lg={6}>
-              <Form.Control type="text" aria-describedby="text-mensaje" />
-              <Form.Text id="text-mensaje"></Form.Text>
-            </Col>
-          </Row>
-        </Form.Group>
-      </Row>
+
       <FormSelect
-        name="Paridad"
+        name="Cantidad de Bits de Datos"
+        values={valuesBitsDatos}
+        defaultValue={defaultBitsDatos}
+        setState={setBitsDatos}
+      />
+
+      <FormSelect
+        name="Cantidad de Bits de Parada"
+        values={valuesBitsParada}
+        defaultValue={defaultBitsParada}
+        setState={setBitsParada}
+      />
+
+      <FormSelect
+        name="Tipo de Paridad"
         values={valuesParidad}
         defaultValue={defaultParidad}
         setState={setParidad}
       />
 
-      <FormSelect
-        name="Parada"
-        values={valuescantidadBitParada}
-        defaultValue={defaultcantidadBitParada}
-        setState={setParada}
+      <FormBtnGroup 
+        name="Pulsadores"
+        state={pulsadores}
+        setState={setPulsadores}
       />
-      <Row>
-        { cambio ? (<Col className="text-center">
-        <Button variant="primary" type="submit">
-          Iniciar experiencia
-        </Button>
-        </Col>):null
 
-        }
+      <FormText 
+        name="Cadena a transmitir"
+        limit={100}
+        state={mensaje}
+        setState={setMensaje}
+      />
+      
+      <Row>
+        { cambio ? (
+          <Col className="text-center d-grid gap-2">
+            <Button variant="primary" type="submit">
+              Iniciar ensayo
+            </Button>
+          </Col>
+        ) : (
+          <Col className="text-center d-grid gap-2">
+            <Button disabled variant="primary" type="submit">
+              Iniciar ensayo
+            </Button>
+          </Col>
+        ) }
+          
+        <Col className="text-center d-grid gap-2">
+          <BtnDownloadImage />
+        </Col>
         
-        <Col className="text-center">
-        <FormSaveUART
-        idUsuario={idUsuario}
-        velocidad={velocidad}
-        cantidadBitDato={cantidadBitDato}
-        paridad={paridad}
-        cantidadBitParada={cantidadBitParada}
-        mensaje={mensaje}
-        />
-        </Col>
-        <Col className="text-center">
-        <DownloadImage
-        />
-        </Col>
       </Row>
     </Form>
-  );
+  )
 }
 
-export default FormUART;
+export default FormUART
--- src/pages/digital/TxRxUART.jsx
+++ src/pages/digital/TxRxUART.jsx
@@ -1,44 +1,44 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
-import Card from "react-bootstrap/Card";
-import Nav from "react-bootstrap/Nav";
+import Card from "react-bootstrap/Card"
+import Nav from "react-bootstrap/Nav"
 
-import LabInformation from "../../components/common/LabInformation";
-import LabVideoStreaming from "../../components/LabVideoStreaming";
+import LabInformation from "../../components/common/LabInformation"
+import LabVideoStreaming from "../../components/LabVideoStreaming"
 
-import FormUART from "../../components/digital/FormUART";
+import FormUART from "../../components/digital/FormUART"
 
-import TableQueryPaginated from "../../components/common/TableQueryPaginated";
+import TableQueryPaginated from "../../components/common/TableQueryPaginated"
 import ExportResults from "../../components/common/ExportResults"
 
-import { useInfoLaboratorio, useEnsayosUsuario, useEnsayos } from "../../hooks/digital";
+import { useInfoLaboratorio, useEnsayosUsuario, useEnsayos } from "../../hooks/digital"
 
-import { headersUART as tableHeaders } from "../../libs/tableHeaders";
+import { headersUART as tableHeaders } from "../../libs/tableHeaders"
 
-import imgUART from "../../assets/uart.png";
-
+import imgUART from "../../assets/uart.png"
 
 /**
- * @return Pagina del laboratorio de Enlace Wifi
+ * 
  */
 function TxRxUART() {
-  const [showForm, setShowForm] = useState(true);
-  const [showResults, setShowResults] = useState(false);
+  const [showForm, setShowForm] = useState(true)
+  const [showResults, setShowResults] = useState(false)
 
-  const idLabActual = 1;
-  const idUsuarioActual = 2;
-  const prof = true;//definir con atilio como me lo manda para saber que es un profesor de fisica y no de otra area;
+  const idLabActual = 1
+  const idUsuarioActual = 2
+
+  const prof = true //definir con atilio como me lo manda para saber que es un profesor de fisica y no de otra area
 
   const onClickTabs = () => {
-    setShowForm(!showForm);
-    setShowResults(!showResults);
-  };
+    setShowForm(!showForm)
+    setShowResults(!showResults)
+  }
 
-  const [componentRef, setComponentRef] = useState({});
+  const [componentRef, setComponentRef] = useState({})
   /**
    * -----------------------------------------------------
    * Renderizado del componente
@@ -110,6 +110,7 @@ function TxRxUART() {
                 </Card>
               ) : null}
             </Card.Body>
+
             <Card.Footer>
               <ExportResults 
                 useHook={useEnsayosUsuario}
@@ -125,7 +126,7 @@ function TxRxUART() {
         </Col>
       </Row>
     </Container>
-  );
+  )
 }
 
-export default TxRxUART;
+export default TxRxUART
