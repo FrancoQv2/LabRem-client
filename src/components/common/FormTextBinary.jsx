864225489315bb07932c0acea2393e42a803c14a
Franco Quevedo <francoq96@gmail.com>
2023-05-28 00:19:50 -0300

    Agregados componentes de Formularios para ingresar solamente en formato hexa o binario

---
+++ src/components/common/FormTextBinary.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Form from "react-bootstrap/Form"
+
+function FormTextBinary({ name, limit, state, setState, disabled=false }) {
+  const handleTextChange = (changeEvent) => {
+    const value = changeEvent.target.value
+    const binaryValue = value.replace(/[^01]/g, "") // Filtrar solo caracteres binarios
+
+    if (binaryValue.length <= limit) {
+      setState(binaryValue)
+    } else {
+      setState(binaryValue.substring(0, limit))
+    }
+  }
+
+  return (
+    <Row className="my-3">
+      <Form.Group
+        className="border border-secondary rounded"
+        controlId={`form-${name.toString().toLowerCase().replace(/ /g, "-")}`}
+      >
+        <Row className="my-3">
+          <Col sm={4} lg={6}>
+            <span
+              className="input-group-text"
+              htmlFor={name.toString().toLowerCase().replace(/ /g, "-")}
+            >
+              {name}
+              {/* {` - (${state.length} / ${limit})`} */}
+            </span>
+          </Col>
+          <Col sm={4} lg={6}>
+            <Form.Control
+              type="text"
+              aria-describedby="text-state"
+              value={state}
+              onChange={handleTextChange}
+              disabled={disabled}
+            />
+            <Form.Text id="text-state"></Form.Text>
+          </Col>
+        </Row>
+      </Form.Group>
+    </Row>
+  )
+}
+
+export default FormTextBinary
---
+++ src/components/common/FormTextHexa.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Form from "react-bootstrap/Form"
+
+function FormTextHexa({ name, limit, state, setState }) {
+  const handleTextChange = (changeEvent) => {
+    const value = changeEvent.target.value
+    const hexValue = value.replace(/[^a-fA-F0-9]/g, "") // Filtrar solo caracteres hexadecimales
+
+    if (hexValue.length <= limit) {
+      setState(hexValue)
+    } else {
+      setState(hexValue.substring(0, limit))
+    }
+  }
+
+  return (
+    <Row className="my-3">
+      <Form.Group
+        className="border border-secondary rounded"
+        controlId={`form-${name.toString().toLowerCase().replace(/ /g, "-")}`}
+      >
+        <Row className="my-3">
+          <Col sm={4} lg={6}>
+            <span
+              className="input-group-text"
+              htmlFor={name.toString().toLowerCase().replace(/ /g, "-")}
+            >
+              {name}
+              {/* {` - (${state.length} / ${limit})`} */}
+            </span>
+          </Col>
+          <Col sm={4} lg={6}>
+            <Form.Control
+              type="text"
+              aria-describedby="text-state"
+              value={state}
+              onChange={handleTextChange}
+            />
+            <Form.Text id="text-state"></Form.Text>
+          </Col>
+        </Row>
+      </Form.Group>
+    </Row>
+  )
+}
+
+export default FormTextHexa
