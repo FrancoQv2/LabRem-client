c560f8526dcdf0ca6819fc02708c521300e0eefb
Franco Quevedo <fquevedo@onebittech.com>
2023-10-20 01:10:28 -0300

    fix: corregido FileUpload

--- src/components/common/FileUpload.jsx
+++ src/components/common/FileUpload.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-// const URL = 'http://localhost:3034/upload'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-function FileUpload({URL}) {
+import { submitSuccess, submitError } from "@libs/alerts"
+
+
+function FileUpload({ URL }) {
   const [selectedFile, setSelectedFile] = useState(null)
 
   const handleFileChange = (e) => {
@@ -14,19 +20,39 @@ function FileUpload({URL}) {
     const formData = new FormData()
     formData.append('file', selectedFile)
 
-    axios.post(URL, formData)
+    axios.post(URL, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
       .then(response => {
         console.log('File uploaded successfully')
+        console.log(response)
+        submitSuccess(response.data)
       })
       .catch(error => {
         console.error('File upload failed', error)
+        submitError('File upload failed')
       })
   }
 
   return (
     <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleFileUpload}>Upload File</button>
+      <hr />
+      <Row className="mt-3">
+        <Col sm={12} md={12} lg={12}>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Suba un archivo .osf</Form.Label>
+            <Form.Control type="file" onChange={handleFileChange} />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="mb-3 d-flex align-items-end">
+        <Col sm={12} md={12} lg={12}>
+          <Button size='sm' onClick={handleFileUpload}>Upload File</Button>
+        </Col>
+      </Row>
+      <hr />
     </div>
   )
 }
