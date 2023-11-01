8d22ff0b17f1b056c151b2cbde376ff8de3eb8b5
Franco Quevedo <francoq96@gmail.com>
2023-11-01 11:01:15 -0300

    fix: actualizado manejo de token en teleco-radio

--- src/modules/teleco-radio/EnlaceRadio.jsx
+++ src/modules/teleco-radio/EnlaceRadio.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react'
+import { useContext, useState } from 'react'
 import { UserContext } from '@context/UserContext'
 
 import Container from 'react-bootstrap/Container'
@@ -21,10 +21,8 @@ import { headersRadio as tableHeaders } from '@libs/tableHeaders'
 
 import imgRadio from '@assets/teleco_radio.png'
 
-// import { useLocation } from 'react-router-dom'
-// import { jwtDecode } from 'jwt-decode'
-
-import TokenProvider from '@context/TokenProvider'
+import { useLocation } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
 
 /**
  *
@@ -43,33 +41,26 @@ function EnlaceRadio() {
     setShowResults(!showResults)
   }
 
-  const [informacion, setInformacion] = useState(null)
+  // Obtencion y decodificacion de token por parametro URL
+  const location = useLocation()
+  const token = new URLSearchParams(location.search).get('token')
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem('informacion')
-    setInformacion(storedToken)
-  }, [])
+  let informacion
+  if (!token) {
+    console.log('Token no encontrado en la URL')
+  } else {
+    try {
+      informacion = jwtDecode(token)
+    } catch (error) {
+      console.error('Error al decodificar el token:', error)
+    }
+    localStorage.setItem('token', token)
+    localStorage.setItem('informacion', JSON.stringify(informacion))
 
-  // // Obtencion y decodificacion de token por parametro URL
-  // const location = useLocation()
-  // const token = new URLSearchParams(location.search).get('token')
-
-  // let informacion
-  // if (!token) {
-  //   console.log('Token no encontrado en la URL')
-  // } else {
-  //   try {
-  //     informacion = jwtDecode(token)
-  //   } catch (error) {
-  //     console.error('Error al decodificar el token:', error)
-  //   }
-  //   localStorage.setItem('token', token)
-  //   localStorage.setItem('informacion', JSON.stringify(informacion))
-
-  //   // Elimina el parámetro 'token' de la URL
-  //   const baseURL = window.location.pathname
-  //   window.history.replaceState({}, document.title, baseURL)
-  // }
+    // Elimina el parámetro 'token' de la URL
+    const baseURL = window.location.pathname
+    window.history.replaceState({}, document.title, baseURL)
+  }
 
   /**
    * -----------------------------------------------------
@@ -77,65 +68,63 @@ function EnlaceRadio() {
    * -----------------------------------------------------
    */
   return (
-    <TokenProvider>
-      <Container className='justify-content-center align-items-center my-4 border border-dark rounded'>
-        <LabInformation
-          imagen={imgRadio}
-          idLaboratorio={idLaboratorio}
-          useInfoLaboratorio={useInfoLaboratorio}
-        ></LabInformation>
-        <hr />
+    <Container className='justify-content-center align-items-center my-4 border border-dark rounded'>
+      <LabInformation
+        imagen={imgRadio}
+        idLaboratorio={idLaboratorio}
+        useInfoLaboratorio={useInfoLaboratorio}
+      ></LabInformation>
+      <hr />
 
-        <Row className='m-2 d-flex justify-content-center'>
-          <Col sm={12} lg={5}>
-            <LabVideoStreaming streamUrl={URL_CAMARA} className='m-2' />
-          </Col>
+      <Row className='m-2 d-flex justify-content-center'>
+        <Col sm={12} lg={5}>
+          <LabVideoStreaming streamUrl={URL_CAMARA} className='m-2' />
+        </Col>
 
-          <Col sm={12} lg={7}>
-            <Card>
-              <FormHeader onClickTabs={onClickTabs} showForm={showForm} showResults={showResults} />
+        <Col sm={12} lg={7}>
+          <Card>
+            <FormHeader onClickTabs={onClickTabs} showForm={showForm} showResults={showResults} />
 
-              <Card.Body>
-                {showForm ? (
-                  <Card id='lab-form'>
-                    <Card.Body>
-                      <FormRadio idUsuario={informacion.usuario.idUsuario} />
-                    </Card.Body>
-                  </Card>
-                ) : null}
+            <Card.Body>
+              {showForm ? (
+                <Card id='lab-form'>
+                  <Card.Body>
+                    <FormRadio idUsuario={informacion.usuario.idUsuario} />
+                  </Card.Body>
+                </Card>
+              ) : null}
 
-                {showResults ? (
-                  <Card id='lab-results'>
-                    <Card.Body>
-                      <TableQueryPaginated
-                        idLaboratorio={idLaboratorio}
-                        idUsuario={informacion.usuario.idUsuario}
-                        tableHeaders={tableHeaders}
-                        useHook={useEnsayosUsuario}
-                        setComponentRef={setComponentRef}
-                      />
-                    </Card.Body>
-                  </Card>
-                ) : null}
-              </Card.Body>
+              {showResults ? (
+                <Card id='lab-results'>
+                  <Card.Body>
+                    <TableQueryPaginated
+                      idLaboratorio={idLaboratorio}
+                      idUsuario={informacion.usuario.idUsuario}
+                      tableHeaders={tableHeaders}
+                      useHook={useEnsayosUsuario}
+                      setComponentRef={setComponentRef}
+                    />
+                  </Card.Body>
+                </Card>
+              ) : null}
+            </Card.Body>
 
-              <Card.Footer>
-                <ExportResults
-                  useHook={useEnsayosUsuario}
-                  exportToProfe={useEnsayos}
-                  idLaboratorio={idLaboratorio}
-                  idUsuario={informacion.usuario.idUsuario}
-                  esProfesor={esProfesor}
-                  // esProfesor={informacion.usuario.rol[0].rol}
-                  filename={'ensayos-radio'}
-                  componentRef={componentRef}
-                />
-              </Card.Footer>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </TokenProvider>
+            <Card.Footer>
+              <ExportResults
+                useHook={useEnsayosUsuario}
+                exportToProfe={useEnsayos}
+                idLaboratorio={idLaboratorio}
+                idUsuario={informacion.usuario.idUsuario}
+                esProfesor={esProfesor}
+                // esProfesor={informacion.usuario.rol[0].rol}
+                filename={'ensayos-radio'}
+                componentRef={componentRef}
+              />
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
